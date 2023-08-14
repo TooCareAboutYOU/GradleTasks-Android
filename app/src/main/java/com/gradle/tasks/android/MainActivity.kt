67aa885e7361505c18dd1ebf5d5d2ba4e97cf2b9
zhangshuai <zhangshuai@attrsense.com>
2023-08-14 16:54:03 +0800

    添加设备唯一性

--- app/src/main/java/com/gradle/tasks/android/MainActivity.kt
+++ app/src/main/java/com/gradle/tasks/android/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.gradle.tasks.android
 
+import android.media.MediaDrm
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.gradle.tasks.android.databinding.ActivityMainBinding
+import java.util.UUID
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,16 +14,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.sampleText.text = stringFromJNI()
+//        binding.sampleText.text = stringFromJNI()
+
+        try {
+            val WIDEVINE_UUID = UUID(-0x121074568629b532L, -0x5c37d8232ae2de13L)
+            val mediaDrm = MediaDrm(WIDEVINE_UUID)
+            val widevineId = mediaDrm.getPropertyByteArray(MediaDrm.PROPERTY_DEVICE_UNIQUE_ID)
+            val sb = StringBuilder()
+            widevineId.forEach {
+                sb.append(String.format("%02x", it))
+            }
+            binding.sampleText.text = sb
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
     }
 
-
-    /**
-     * ---------------------------------------------------------------------------------------------
-     *
-     * ---------------------------------------------------------------------------------------------
-     * description： JNI
-     */
     companion object {
         init {
             System.loadLibrary("android")
